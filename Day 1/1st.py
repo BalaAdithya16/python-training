6b39f59aa41d7ee61086442ffe0f70f94450fcae
Adithya <vacchaadithya@gmail.com>
2025-05-23 11:51:55 +0530

    training

---
+++ Day 1/1st.py
@@ -0,0 +1,18 @@
+# import keyword
+# print("list of keywords:",len(keyword.kwlist))
+
+
+# #it prints telugu alphabets
+# for i in range(3077,3100):
+#     print(chr(i))
+    
+
+a="hba"
+b="hba"
+# a and b has same memory id
+print(id(a))
+print(id(b))
+
+
+#-(n+1) bitwise operator
+# ex: n=6 then (~n)=-7
---
+++ Day 1/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+import keyword
+# print("list of keywords:",len(keyword.kwlist))
+
+
+# #it prints telugu alphabets
+# for i in range(3077,3100):
+#     print(chr(i))
