4265e823780267a05a9cd2b825b030482ee977a8
V.BalaAdithya <vacchaadithya@gmail.com>
2025-05-24 10:47:06 +0530

    collection datatypes.py

--- Day 1/collection datatypes.py
+++ Day 1/collection datatypes.py
@@ -1,10 +1,10 @@
 # list - [] - It is a collection of different datatypes sequal ordered,mutable,
 #             indexed and supports duplicate values. we can create a list with
 #             square bracket.Lists are dynamic     
-# tuple - ()
+# tuple - () - A tuple is a collection of different datatypes which is indexed immutable
+#              and allow duplicate value. We can create tuple using round bracket 
 # set - {}
 # dictionary - {key:value}  All stores hetrogeneous datatype elements
 
 li=[10,"adhi",20.23]
 print(li[2]) #[2] is subscript operator
-
