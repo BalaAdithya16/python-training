992d79ab2292f2df66481c4d3266bc5cde7ae110
V.BalaAdithya <vacchaadithya@gmail.com>
2025-05-24 09:43:50 +0530

    Add files via upload

---
+++ Day 1/collection datatypes.py
@@ -0,0 +1,10 @@
+# list - [] - It is a collection of different datatypes sequal ordered,mutable,
+#             indexed and supports duplicate values. we can create a list with
+#             square bracket.Lists are dynamic     
+# tuple - ()
+# set - {}
+# dictionary - {key:value}  All stores hetrogeneous datatype elements
+
+li=[10,"adhi",20.23]
+print(li[2]) #[2] is subscript operator
+
